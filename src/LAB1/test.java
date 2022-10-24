ec7e473f87ad9a4af36285e43179f73d8770f79b
YUZUq <116411183+YUZUq@users.noreply.github.com>
2022-10-24 22:09:47 +0800

    Update test.java

--- src/LAB1/test.java
+++ src/LAB1/test.java
@@ -19,16 +19,16 @@ public class test {
 	@ParameterizedTest
     @MethodSource("parameterDataProvider")
     void test(String url, int l, String result) throws IOException {
-        t.sol(url, l);
+        t.read(url);
         assertEquals(result, t.toString());
     }
 
     public static Stream<Arguments> parameterDataProvider() {
         return Stream.of(
-                Arguments.of("C:\\Users\\ZYC\\Desktop\\source.c", 1, "total num: 35"),
-                Arguments.of("C:\\Users\\ZYC\\Desktop\\source.c", 2, "switch num: 2\ncase num: 3 2"),
-                Arguments.of("C:\\Users\\ZYC\\Desktop\\source.c", 3, "if-else num: 2"),
-                Arguments.of("C:\\Users\\ZYC\\Desktop\\source.c", 4, "if-elseif-else num: 2")
+                Arguments.of("C:\\Users\\sunfu\\Desktop\\codeExample.c", 1, "total num: 35"),
+                Arguments.of("C:\\Users\\sunfu\\Desktop\\codeExample.c", 2, "switch num: 2\ncase num: 3 2"),
+                Arguments.of("C:\\Users\\sunfu\\Desktop\\codeExample.c", 3, "if-else num: 2"),
+                Arguments.of("C:\\Users\\sunfu\\Desktop\\codeExample.c", 4, "if-elseif-else num: 2")
         );
     }
 }
